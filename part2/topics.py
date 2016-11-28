eefd5e1391cc455a30d284d5e65a80f74ccff278
singhgag <singhgag@indiana.edu>
2016-11-28 18:26:50 -0500

    Confusion matrix and comments

--- part2/topics.py
+++ part2/topics.py
@@ -11,16 +11,16 @@ def make_model(directory):
 	dir_list =  listdir(directory)
 	total_words_under_all = 0
 	for d in dir_list:
-		freq_dict.update({d:{}})
+		freq_dict.update({d:{}})#initializing keys with empty values in the dictionary
 		path = directory+"/"+d
 		total_words = 0
 		for root,dirs,files in os.walk(path):
 			for name in files:
 				print d,name
-				total_words = total_words + calculate_from_file(d,path+"/"+name)
-		freq_dict.update({"total words under "+d:total_words})
-		total_words_under_all = total_words_under_all + total_words
-	freq_dict.update({"total words under all": total_words_under_all})
+				total_words = total_words + calculate_from_file(d,path+"/"+name)  #total words in a particular file under the topic directory
+		freq_dict.update({"total words under "+d:total_words}) #updating the dictionary to reflect the total words under a particular topic
+		total_words_under_all = total_words_under_all + total_words #total words under all files under a topic
+	freq_dict.update({"total words under all": total_words_under_all}) #updating the dictionary to reflect total words under all titles
 
 #opens a file and reads it line by line, calculating and recording/updating word frequencies as it goes
 def calculate_from_file(d, filepath):
@@ -29,11 +29,11 @@ def calculate_from_file(d, filepath):
 	words_in_file = 0
 	for line in f:
 		cleanLine = re.sub('\W+',' ', line )
-		wordlist = cleanLine.split()
-		lowercaselist = [x.lower() for x in wordlist]
-		wordfreq = [wordlist.count(p) for p in lowercaselist]
-    		freq_dict[d].update(dict(zip(lowercaselist,wordfreq)))
-		words_in_file = words_in_file+sum(wordfreq)
+		wordlist = cleanLine.split()	#getting rid of all special chars 
+		lowercaselist = [x.lower() for x in wordlist]	#converting all proper words to lower case for uniformity
+		wordfreq = [wordlist.count(p) for p in lowercaselist] #calculating word frequencies in the line 
+    		freq_dict[d].update(dict(zip(lowercaselist,wordfreq)))	#updating frequencies in the dictionary
+		words_in_file = words_in_file+sum(wordfreq)	#calculating total words in the file
 	return words_in_file
 #revise model to include probabilities of words given title
 def revise_model():
@@ -47,20 +47,31 @@ def revise_model():
 #predicts the topic based on MLE							
 def predict_topic(directory, loaded_model):
  	dir_list =  listdir(directory)
-	topic_dict={}
-        for d in dir_list:
+	topic_dict={} #keeps count of words predicted to belong to a certain topic in a certain file
+	index_dict={} #indicating which index in the confusion matrix belongs to which topic
+	count = 0 #just a counter to help in identifying indexes corresponding to topics in the confusion matrix
+	w = 20 #length and width of the confusion matrix - given assumption in problem statement - total number of topics is 20 
+	conf_mtr = [[0 for x in range(w)] for y in range(w)] #intializing confusion matrix	
+
+	#intialising confusion matrix
+	for dr in dir_list:
+        	index_dict.update({dr:count})
+		count+=1
+
+        #traversing test data to predict topic
+	for d in dir_list:
                 path = directory+"/"+d
                 for root,dirs,files in os.walk(path):
                         for name in files:
                                 for d1 in dir_list:
-					topic_dict.update({d1:0})
+					topic_dict.update({d1:0}) #updating the dictionary to reflect number of word matches to corresponding topics  
 				print d,name
                                 f = open(path+"/"+name, 'r')
         			for line in f:	
-                			cleanLine = re.sub('\W+',' ', line )
-                			wordlist = cleanLine.split()
+                			cleanLine = re.sub('\W+',' ', line ) #cleaning line to exclude special chars 
+                			wordlist = cleanLine.split() #getting proper words from the line
 					for p in wordlist:
-						max_val = float(1)/10**50
+						max_val = float(1)/10**50 #random value assumed for comparison
 						topic=d
 						for key in loaded_model:
                 					if not key.startswith( 'total', 0, 5 ):
@@ -75,7 +86,16 @@ def predict_topic(directory, loaded_model):
 					if topic_dict[d2] > max_cnt and topic_dict[d2] < 80:
 						max_cnt = topic_dict[d2]
 						topic = str(d2)
-				print "Topic predicted - ", topic	
+				print "Topic predicted - ", topic
+				if topic != d:
+					conf_mtr[index_dict[d]][index_dict[topic]]+=1 #"adding to the confusion"
+	#printing confusion matrix
+	for x in range(20):
+		temp = ""
+		for y in range(20):
+			temp = temp + str(conf_mtr[x][y])+" "
+		print temp+"\n"		
+	
 def findTopTen():
 	top_dict={}
 	f = open('distinctive_words.txt', 'w')
